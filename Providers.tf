1a2db39d7b258bda0c09adcde48c0190b4f9c460
DarK <sagarsinghrajpoot@Sagars-MacBook-Pro.local>
2020-03-16 23:17:13 +0100

    mac_setup

--- Providers.tf
+++ Providers.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "sagar-terraform"
+    bucket         = "sagar-terraform-mac"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "sagar-terraform"
